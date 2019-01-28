44d6241de0255102f18f87836b2cabc006d7bc83
Nicolas TERRONE <nicolasterrone@gmail.com>
2019-01-28 22:12:21 +0100

    Added Motion effect to ParallaxCell

--- parallax_test/View/ParallaxCell.swift
+++ parallax_test/View/ParallaxCell.swift
@@ -15,7 +15,7 @@ class ParallaxCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setupParallax()
     }
 
     public func updateCell(withItem item: Item){
@@ -24,7 +24,20 @@ class ParallaxCell: UITableViewCell {
     }
     
     func setupParallax(){
+        let min = CGFloat(-30)
+        let max = CGFloat(30)
         
+        let xMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.x", type: .tiltAlongHorizontalAxis)
+        xMotion.maximumRelativeValue = max
+        xMotion.minimumRelativeValue = min
+        
+        let yMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.y", type: .tiltAlongVerticalAxis)
+        yMotion.maximumRelativeValue = max
+        yMotion.minimumRelativeValue = min
+        
+        let effectGroup = UIMotionEffectGroup()
+        effectGroup.motionEffects = [xMotion, yMotion]
+        
+        imageCell.addMotionEffect(effectGroup)
     }
-
 }
